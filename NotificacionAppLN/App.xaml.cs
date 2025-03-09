76fe47fca74d3c24e0e6fa4f51cdb84003e20653
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-09 08:54:35 -0500

    Add project files.

---
+++ NotificacionAppLN/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotificacionAppLN
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NotificacionAppLN/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificacionAppLN
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
